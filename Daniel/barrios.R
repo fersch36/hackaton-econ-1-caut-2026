c20b2642beb918bef686de79b0b5f2cb67bcd6bf
danielgamarra2003 <danielgamarra2003@gmail.com>
2026-04-17 19:44:47 -0300

    barrios

--- Daniel/barrios.R
+++ Daniel/barrios.R
@@ -1,1 +1,13 @@
 
+
+library(sf)
+library(ggplot2)
+
+
+#https://cdn.buenosaires.gob.ar/datosabiertos/datasets/innovacion-transformacion-digital/barrios/barrios.geojson
+
+#descarga de datos de los barrios de CABA
+caba <- st_read( "CS_DE_DATOS/Repo-curso-2026C1/barrios.geojson.txt")
+
+ggplot(data = caba) +
+  geom_sf()
